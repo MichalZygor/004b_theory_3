4d4781cf4bc7214bd4501b1b8f5dbe9e579e9929
Michal Zygor <michal@winami.com>
2020-10-12 21:11:35 +0200

    004b_theory_3: enkapsulacja - wykonanie

--- src/DiscountApp.java
+++ src/DiscountApp.java
@@ -1,22 +1,22 @@
 public class DiscountApp {
     public static void main(String[] args) {
-        String firstName = "jan";
-        String lastName = "kowalski";
-        boolean clientPremium = true;
+        Client client1 = new Client("Jan", "Kowalski", true);
         double price = 1100; //PLN
-        double discountPrice = price;
+        DiscountService discountService = new DiscountService();
+        double discountPrice = discountService.calculateDiscountPrice(client1, price);
 
-        if (clientPremium && price > 1000) {
-            discountPrice *= 0.85;
-        }else if (price > 1000){
-            discountPrice *= 0.9;
-        }else if (clientPremium){
-            discountPrice *= 0.95;
-        }
-
-        System.out.println("Witaj " + firstName + " " + lastName);
+        System.out.println("Witaj " + client1.getFirstName() + " " + client1.getLastName());
         System.out.println("Kwota przed rabatem: " + price);
         System.out.println("Kwota po rabacie: " + discountPrice);
+
+
+        Client client2 = new Client("Karol", "Nowak", false);
+        double price2 = 1100; //PLN
+        double discountPrice2 = discountService.calculateDiscountPrice(client2, price2);
+
+        System.out.println("Witaj " + client2.getFirstName() + " " + client1.getLastName());
+        System.out.println("Kwota przed rabatem: " + price2);
+        System.out.println("Kwota po rabacie: " + discountPrice2);
 
 
     }
